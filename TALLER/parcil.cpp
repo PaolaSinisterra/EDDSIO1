8bd480e8a2a1e4abc85d8a123890151cad180b51
PaolaSinisterra <psinisterra889@gmail.com>
2024-03-16 15:44:13 -0500

    metodo mostrar

--- TALLER/parcil.cpp
+++ TALLER/parcil.cpp
@@ -13,22 +13,49 @@ struct Producto {
 struct nodo
 {
   Producto producto;
-  nodo* siguiente
+  nodo* siguiente;
 };
 
 nodo* a=NULL;
 nodo* b=NULL;
 
 int registar(){
-    nodo* nuevonodo=(nodo*)malloc(sizeof(nodo));
+    nodo* cabeza=(nodo*)malloc(sizeof(nodo));
 
     cout<<"\nIngrese el ID del producto a registrar: ";
-    cin>>nuevonodo->producto.id;
+    cin>>cabeza->producto.id;
 
-    cout<<""
+    cout<<"Ingrese el nombre del producto";
+    cin>>cabeza->producto.nombre;
 
+    cout<<"Ingrese la cantidad: ";
+    cin>>cabeza->producto.cantidad;
+
+    cout<<"Ingrese el precio por unidad del producto: ";
+    cin>>cabeza->producto.precio;
+
+    cabeza->siguiente=NULL;
+
+    if (b==NULL)
+    {
+        a=b=cabeza;
+    }else{
+        b->siguiente=cabeza;
+        b=cabeza;
+    }
+    
+    
+cout<<"El producto se registro exitosamente.\n";
 
 return 0;
+}
+
+int mostrar(){
+    nodo* produc=a;
+
+    if(produc==NULL){
+        cout<<
+    }
 }
 
 
